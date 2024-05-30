e3fd36444a83cd21adf2ad7293e3796de85f36fe
yuyang <yuyangxxxmail@gmail.com>
2024-05-30 16:20:41 +0800

    somehow passed

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,20 @@ use postgis_sys::mvt_geom as raw_mvt_geom;
 
 pub fn mvt_geom(
     geom: &lwgeom::LWGeom,
-    gbox: &lwgeom::GBox,
+    gbox: &lwgeom_sys::GBOX,
     extend: u32,
     buffer: u32,
     clip_geom: bool,
 ) -> lwgeom::LWGeom {
-    let x = unsafe { raw_mvt_geom(geom.as_ptr(), gbox.as_ptr(), extend, buffer, clip_geom) };
+    let x = unsafe {
+        raw_mvt_geom(
+            geom.as_ptr(),
+            gbox as *const lwgeom_sys::GBOX,
+            extend,
+            buffer,
+            clip_geom,
+        )
+    };
     lwgeom::LWGeom::from_ptr(x)
 }
 
@@ -31,12 +39,14 @@ mod tests {
 
         let x = mvt_geom(
             &lwgeom::LWGeom::from_ewkt("POLYGON ((0 0, 10 0, 10 5, 0 -5, 0 0))").unwrap(),
-            &lwgeom::GBox::from_ptr(&gbox as *const lwgeom_sys::GBOX as *mut lwgeom_sys::GBOX),
+            &gbox,
             4096,
             0,
             false,
         );
-
-        println!("{}", x.as_ewkt(None).unwrap());
+        assert_eq!(
+            "MULTIPOLYGON(((5 4096,10 4091,10 4096,5 4096)),((5 4096,0 4101,0 4096,5 4096)))",
+            x.as_ewkt(None).unwrap()
+        )
     }
 }
